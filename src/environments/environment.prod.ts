252326af79d8381fdf73b1f0d93fc8de6bff6ad7
Dylan Dupasquier <dylandupasquier@gmail.com>
2023-05-01 10:37:48 -0700

    LETS TRY THIS AGAIN

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    supabaseUrl: 'https://qrbojeutgvnbkbkgkxvb.supabase.co',
+    supabaseKey: 'SUPABASE_KEY',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://qrbojeutgvnbkbkgkxvb.supabase.co',
+    supabaseKey: 'SUPABASE_KEY',
+};
