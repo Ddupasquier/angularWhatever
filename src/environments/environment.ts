4661a50b1d95cc98414e50d35f259d73f3f758ba
Dylan Dupasquier <dylandupasquier@gmail.com>
2023-05-01 10:41:21 -0700

    okay...

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     supabaseUrl: 'https://qrbojeutgvnbkbkgkxvb.supabase.co',
-    supabaseKey: 'SUPABASE_KEY',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFyYm9qZXV0Z3ZuYmtia2dreHZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI5MDYzOTUsImV4cCI6MTk5ODQ4MjM5NX0.5TLLjC721LSq14mW7KNdT9CPu6ZY1VXiVOh9wNG1RW8',
 };
